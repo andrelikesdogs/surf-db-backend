b30c9eb6dcc3bf33bef6bb52eb132564e6c158aa
Andre <andre.meyer93+alt@gmail.com>
2021-10-27 14:47:06 +0200

    fix missing host for mysql config

--- src/entities/mysql.js
+++ src/entities/mysql.js
@@ -4,6 +4,7 @@ const sequelize = new Sequelize({
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: "mysql",
 });
